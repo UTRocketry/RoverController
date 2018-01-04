5e8f937da16909ea4ddc38defd302fdb4b2c4aa8
Luke9978 <lpalumbo99@gmail.com>
2018-01-03 20:19:11 -0500

    Where did clock_div_1 go

--- UTR.c
+++ UTR.c
@@ -10,6 +10,8 @@
 #define ENCODING_W    0X091
 #define FRAMING_R     0X012
 #define FRAMING_W     0X092
+#define RADIOSTATE    0x01C
+#define FIFOCMD       0x0A8 //Five bit command to commit data to the FIFO to send 0bXXX00000
 #define FIFOSTAT      0X028
 #define FIFODATA_R    0X029
 #define FIFOCOUNT1_R  0X02A
@@ -29,15 +31,9 @@
 #define WORRX      0b01101011
 #define SYNTHTX    0b01101100
 #define FULLTX     0b01101101
+#define EMPTY      0b00000000
 
-bool initRaido(unsigned char freq) {
-	//SPI_RW_8(POWERMODE_W, STANDBY); // Put radio into stand by 
-	return true;
-}
 
-void restartRadio() {
-
-}
 uint8_t USBint = 0;
 USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface = {
 	.Config =
@@ -65,23 +61,28 @@ USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface = {
 };
 
 static FILE USBSerialStream;
-
+void transferData(void) {
+	/*PORTD |= (1 << DDD0); //set PD0 high
+	uint16_t data = eeprom_read_word (( uint16_t *) 46) ;
+	char dataLSB =  data & 0xFF;
+	char dataMSB = data >> 8;
+	fputs(dataMSB, &USBSerialStream);
+	fputs(dataLSB, &USBSerialStream);
+	PORTD &= ~(1 << DDD0); //set PD0 low*/
+}
 void SetupHardware(void) {
 	MCUSR &= ~(1 << WDRF);
 	wdt_disable();
 	clock_prescale_set(clock_div_1);
 	USB_Init();
 }
-
 void EVENT_USB_Device_Connect(void) {
 	PORTD |= (1 << DDD0); //set PD0 high
 	_delay_ms(100);
 	PORTD &= ~(1 << DDD0); //set PD0 low
 	_delay_ms(100);
 }
-void USBStreamInit(){
-	CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream); //Init USB stream
-}
+
 void EVENT_USB_Device_Disconnect(void) {
 	PORTD |= (1 << DDD0); //set PD0 high
 	_delay_ms(100);
@@ -100,38 +101,16 @@ void EVENT_USB_Device_ControlRequest(void) {
 
 void EVENT_CDC_Device_ControLineStateChanged(USB_ClassInfo_CDC_Device_t *const CDCInterfaceInfo) {
 	bool HostReady = (CDCInterfaceInfo->State.ControlLineStates.HostToDevice & CDC_CONTROL_LINE_OUT_DTR) != 0;
-	if (HostReady)
-		transferData();
+	if (HostReady) { transferData(); }
 }
 
-void USBInterruptjob() {
-	USBint++;
-	//if (USBint > 3){
-	//USBint = 0;
-	CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
-	CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
-	USB_USBTask();
-	//}
-}
 void sendSerial(char rec) {
 	CDC_Device_SendString(&VirtualSerial_CDC_Interface, &rec);
 }
-
-void transferData(void){
-		/*PORTD |= (1 << DDD0); //set PD0 high
-		uint16_t data = eeprom_read_word (( uint16_t *) 46) ;
-		char dataLSB =  data & 0xFF;
-		char dataMSB = data >> 8;
-		fputs(dataMSB, &USBSerialStream);
-		fputs(dataLSB, &USBSerialStream);
-		PORTD &= ~(1 << DDD0); //set PD0 low*/
-}
-
 void SPI_MasterInit(void){
 	DDRB |= (1<<DDB1)|(1<<DDB2)|(1<<DDB0);
 	SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
-
 /*
 MSB of reg_A is RW bit of that register
 
@@ -153,10 +132,46 @@ char SPI_RW_8(unsigned char reg_A,unsigned char reg_D){
 	return SPDR;
 }
 
+bool initRaido(unsigned char freq) {
+	SPI_RW_8(POWERMODE_W, STANDBY); // Put radio into stand by 
+	_delay_ms(50);
+	if (SPI_RW_8(RADIOSTATE, EMPTY) == 0) { return true; }
+	else { return false; }
+	/*
+	The Radio can have 11 possibale states
+	0 - Idle
+	1 - Poweredown
+	4 - Tx PLL Settings
+	6 - TX
+	7 - Tx Tail
+	8 - Rx PLL Settings
+	9 - Rx Antenna Selection
+	12 - Rx Preamble 1	13 - Rx Preamble 2
+	14 - Rx Preamble 3
+	15 - RX
+	*/
+}
+void txRadio(unsigned char message) {
+	SPI_RW_8(FIFOCOUNT0_W, message);
+
+}
+void rssiSignal() {
+	//SPI_RW_8();
+	//return(0);
+	_delay_ms(1);
+}
+void restartRadio() {
+	_delay_ms(1);
+}
 ISR(TIMER0_OVF_vect) { //moved from main loop to timer .1 second / (8Mhz / 1024 prescale) = 12.8
 					   /*HANDLE USB COMMUNICATIONS*/
 	cli();
-	USBInterruptjob();
+	USBint++;
+	//if (USBint > 3){
+	//USBint = 0;
+	CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
+	CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
+	USB_USBTask();
 	sei();
 }
 
@@ -170,18 +185,14 @@ int main(void){
 	PORTB |= (1 << PB0); //SS high
 	sei(); // Set interputs 
 	SetupHardware(); //USB init 
-	USBStreamInit(); //USB stream init
+	CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream); //Init USB stream
 	GlobalInterruptEnable();
 	SPI_MasterInit(); // Turns AVR device into SPI Master
 	initRaido(0);//NEEDS freq 
 	//END OF INIT CODE
 
-
-	//TEST CODE  
-	//char fifo[32];
-	//char fifo_dat; 
 	_delay_ms(1000);
-	//SPI_RW_8(POWERMODE_W,FULLRX);  // Bring the power registers to Full RX
+	sendSerial(initRadio());
 	while(true){
 		char rec = SPI_RW_8(0x028, 0b00000000);
 		//fputs(rec, &USBSerialStream);
