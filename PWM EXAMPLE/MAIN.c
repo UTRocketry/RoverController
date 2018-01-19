69ee3118b7f6be1d98684168262fc9e0436e45be
cmccaskey <mccaskeycameron@gmail.com>
2018-01-18 22:13:08 -0500

    new folders and PWM code

---
+++ PWM EXAMPLE/MAIN.c
@@ -0,0 +1,76 @@
+/*
+atmega16u2
+
+^^^ may need to change this ^^^
+
+Quick PWM reference:
+https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjS7fG28-LYAhVLQq0KHYCvDycQjRx6BAgAEAY&url=https%3A%2F%2Fwww.rcgroups.com%2Fforums%2Fattachment.php%3Fattachmentid%3D2442218&psig=AOvVaw0Jkq-_pxMajvk5OGPhEv7j&ust=1516412637706417
+
+30 Hz Servo Calculations:
+
+1 ms = 0 deg
+1.5 ms = 90 deg
+2 ms = 180 deg
+
+At 30 Hz,
+30 Hz = 30 pulses per second, 1 pulse = 1 / 30 second
+1 / 30 second = .0333 second = 33.33 ms
+Ex: 2ms duty cycle = 2 ms / 33.33 ms = 6%
+
+OC = output compare
+OC_register = 255 - ((1 + (deg / 180)) / 33.33) * 255
+
+PS don't forget to restart the board after programming
+*/
+
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+void initServos() { //don't init until you want servos to become stiff
+	//Config of 8 bit PWM
+	DDRB |= (1 << DDB7); //B7 output, OC.0A
+	DDRD |= (1 << DDD0); //D0 output, OC.0B
+	TCCR0A |= (1 << COM0A1) | (1 << COM0A0) | (1 << COM0B1) | (1 << COM0B0) | (1 << WGM00); //OC.0A & OC.0B set as PWM  //PWM phase correct
+	TCCR0B = (1 << CS00) | (1 << CS01); //Clk / 64 ~ 30Hz checked with meter
+	//Config of 16 bit PWM
+	DDRC |= (1 << DDC6) | (1 << DDC5); //C6 output, OC.1A, C5 output OC.1B
+	TCCR1A |= (1 << COM1A1) | (1 << COM1A0) | (1 << COM1B1) | (1 << COM1B0) | (1 << WGM00); //OC.0A & OC.0B set as PWM  //PWM phase correct
+	TCCR1B = (1 << CS10) | (1 << CS11); //Clk / 64 same as 8 bit ~ 30 Hz
+}
+
+void setServo(int servo, int degree){
+	int OCVal; //OC registers only take an int
+	switch (servo){
+	case 1:
+		OCVal = 255 - ((1 + (degree / 180)) / 33.33) * 255;
+		OCR0A = OCVal;
+		break;
+	case 2:
+		OCVal = 255 - ((1 + (degree / 180)) / 33.33) * 255;
+		OCR0B = OCVal;
+		break;
+	case 3:
+		OCVal = 255 - ((1 + (degree / 180)) / 33.33) * 255;
+		OCR1A = OCVal;
+		break;
+	case 4:
+		OCVal = 255 - ((1 + (degree / 180)) / 33.33) * 255;
+		OCR1B = OCVal;
+		break;
+	}
+
+
+}
+
+int main() {
+	initServos();
+
+	while (1) {
+		setServo(1, 180);
+		setServo(2, 180);
+		setServo(3, 180);
+		setServo(4, 180);
+	  }
+}
