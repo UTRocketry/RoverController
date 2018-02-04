ade6732031649606049700208d362f5d531b5c53
cmccaskey <mccaskeycameron@gmail.com>
2018-02-04 16:51:19 -0500

    PWM works

--- PWM EXAMPLE LUFA/VirtualSerial/UTR.c
+++ PWM EXAMPLE LUFA/VirtualSerial/UTR.c
@@ -1,6 +1,6 @@
 #include "VirtualSerial/VirtualSerial.h"
-#include <avr/eeprom.h>
-#include <avr/interrupt.h>
+//#include <avr/eeprom.h>
+//#include <avr/interrupt.h>
 #include <util/delay.h>
 
 USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface =
@@ -78,11 +78,11 @@ void initServos(void) { //don't init until you want servos to become stiff
 	DDRB |= (1 << DDB7) | (1 << DDB6); //B7 output, OC.0A, B6 output, bit banged PWM
 	DDRD |= (1 << DDD0); //D0 output, OC.0B
 	TCCR0A |= (1 << COM0A1) | (1 << COM0A0) | (1 << COM0B1) | (1 << COM0B0) | (1 << WGM00); //OC.0A & OC.0B set as PWM  //PWM phase correct
-	TCCR0B = (1 << CS00) | (1 << CS01); //Clk / 64 ~ 30Hz checked with meter
+	TCCR0B = (1 << CS02); //~60Hz
 	//Config of 16 bit PWM
 	DDRC |= (1 << DDC6) | (1 << DDC5); //C6 output, OC.1A, C5 output OC.1B
 	TCCR1A |= (1 << COM1A1) | (1 << COM1A0) | (1 << COM1B1) | (1 << COM1B0) | (1 << WGM00); //OC.0A & OC.0B set as PWM  //PWM phase correct
-	TCCR1B = (1 << CS10) | (1 << CS11); //Clk / 64 same as 8 bit ~ 30 Hz
+	TCCR1B = (1 << CS02);
 }
 
 void delay_ms(uint16_t count) {
@@ -97,46 +97,34 @@ void delay_us(uint16_t count) {
   }
 }
 
-void setServo(int servo, int degree){
-	int OCVal; //OC registers only take an int
+//https://cdn-shop.adafruit.com/product-files/2442/FS90R-V2.0_specs.pdf
+//https://servodatabase.com/servo/towerpro/sg90
+#define servoCenter 1.5 //milliseconds
+#define servoRange 0.9 //difference from center
+#define servoPeriod 0.06 //dont know why but this works
+//period is 1 / frequency
+
+void setServo(int servo, int speed){ //speed 0 - 10 positive or negative CW is -, CCW is +
+	float OCVal; //OC registers only take an int
 	switch (servo){ //numbers match with schematic
-	case 1:
-		OCVal = 255 - ((1 + (degree / 180.0)) / 33.33) * 255;
-		OCR1A = OCVal;
-		break;
-	case 2:
-		OCVal = 255 - ((1 + (degree / 180.0)) / 33.33) * 255;
-		OCR1B = OCVal;
-		break;
-	case 3:
-		OCVal = 255 - ((1 + (degree / 180.0)) / 33.33) * 255;
-		OCR0B = OCVal;
-		break;
 	case 4:
-		OCVal = 255 - ((1 + (degree / 180.0)) / 33.33) * 255;
-		OCR0A = OCVal;
+		OCVal = 255 - (servoCenter + servoRange * speed * 0.1) * servoPeriod * 255;
+		OCR0A = (int) OCVal;
 		break;
-	case 5:
-		OCVal = 1 + (degree / 180.0) * 1000; //using delay_us below for better resolution
-		PORTB |= (1 << PB6); //set PB6 high
-		delay_us(OCVal); //wait
-		PORTB &= ~(1 << PB6); //set PB6 low
-		delay_ms(3 - OCVal / 1000); //3.33 ms became 3 ms to make output closer to 30 Hz
-		break;
-	}
-}
+}}
 
 int main(void){
+	clock_prescale_set(clock_div_1);
 	initServos();
-
 	while(true){
-		setServo(1, 180);
-		setServo(2, 180);
-		setServo(3, 180);
-		setServo(4, 180);
-		setServo(5, 180);
-		CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
+		//setServo(1, 180);
+		//setServo(2, 180);
+		//setServo(3, 180);
+		setServo(4, 1);
+		//setServo(4, 135);
+		//delay_ms(2000);
+		//setServo(5, 180);
+		/*CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
 		CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
-		USB_USBTask();
-	}
-}
+		USB_USBTask();*/
+}}
